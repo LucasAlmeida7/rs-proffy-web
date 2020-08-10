023d5a80ee8fc46f23fc42630e12421a10459fc2
Lucas Almeida Paula <magmapt@gmail.com>
2020-08-09 22:11:05 -0300

    Teacher Form and Teacher List upgraded

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 
@@ -10,18 +12,34 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis." >
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da Semana</label>
-                        <input type="text" id="week_day" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
+                <Select 
+                        name="subject" 
+                        label="Matéria" 
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'Educação Física', label: 'Educação Física' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Matemática', label: 'Matemática' }
+                        ]}
+                    />
+                    <Select 
+                        name="week_day" 
+                        label="Dia da Semana" 
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda' },
+                            { value: '2', label: 'Terça' },
+                            { value: '3', label: 'Quarta' },
+                            { value: '4', label: 'Quinta' },
+                            { value: '5', label: 'Sexta' },
+                            { value: '6', label: 'Sábado' },
+                        ]}
+                    />
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
 
